3af101e29df5584a8ae7a81eeefcca9159fe4847
Tjalling Feenstra <tjallingdokkum@gmail.com>
2022-10-30 20:47:31 +0100

    Updated for packagist

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '62d0fc35bc0db850fd4f1729998f2f3dd45449c0',
+        'name' => 'tjall/router',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tjall/router' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '62d0fc35bc0db850fd4f1729998f2f3dd45449c0',
+            'dev_requirement' => false,
+        ),
+    ),
+);
