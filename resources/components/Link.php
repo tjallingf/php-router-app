83f17b7f691c73787fbd53947a2b9a7728db2de2
Tjalling Feenstra <tjallingdokkum@gmail.com>
2023-03-01 12:43:38 +0100

    update docs

--- resources/components/Link.php
+++ resources/components/Link.php
@@ -2,6 +2,6 @@
     use Router\Config; 
     $omitted_props = $props->omit(['to']); 
 ?>
-<a href="/<?= Config::get('router.baseUrl'); ?>/<?= trim($props->to, '/'); ?>" <?= $omitted_props; ?>>
+<a href="<?= Config::get('router.baseUrl'); ?>/<?= trim($props->to, '/'); ?>" <?= $omitted_props; ?>>
     <?= $props->children; ?>
 </a>
